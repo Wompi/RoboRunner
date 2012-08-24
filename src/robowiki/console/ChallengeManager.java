5a75560d4c25ec7a01afc1875a99fa0ec33c4f92
Wompi <robocode@casepool.de>
2012-08-24 23:14:20 +0200

    First challenge files
    Just a quick draft

---
+++ src/robowiki/console/ChallengeManager.java
@@ -0,0 +1,15 @@
+package robowiki.console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChallengeManager
+{
+	private final List<String>	myChallFiles;
+
+	public ChallengeManager()
+	{
+		myChallFiles = new ArrayList<String>();
+	}
+
+}
---
+++ src/robowiki/console/RunnerChallenge.java
@@ -0,0 +1,16 @@
+package robowiki.console;
+
+public class RunnerChallenge
+{
+	private String	myName;
+	private double	myH;
+	private double	myW;
+	private int		myRounds;
+
+	// not really needed because the score type can be changed with the output. 
+	// it collects all data and then you can output whatever you want
+	private String	myScoreType;
+
+	public RunnerChallenge()
+	{}
+}
